bfdbb873ccb7e5118736c70d24219efd6c386c12
yogesh-m-4091 <131531300+yogesh-m-4091@users.noreply.github.com>
2024-04-15 12:54:40 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from './CTA'
-import ME from "../../assets/yogiskybluestanding.jpg"
+import ME from "../../assets/yogi image.jpg"
 import HeaderSocial from './HeaderSocial'
 
 const Header = () => {
